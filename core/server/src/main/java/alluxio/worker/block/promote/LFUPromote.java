aee9c99a20ef4a95b4361a330170878737afd161
Yours-shupeng <a051587782106@163.com>
2017-02-21 14:43:25 +0800

    change double to float

--- core/server/src/main/java/alluxio/worker/block/promote/LFUPromote.java
+++ core/server/src/main/java/alluxio/worker/block/promote/LFUPromote.java
@@ -39,9 +39,9 @@ public class LFUPromote extends AbstractPromote {
   // Map from block id to the last updated logic time count
   private final Map<Long, Long> mBlockIdToLastUpdateTime = new ConcurrentHashMapV8<>();
   // Map from block id to the CRF value of the block
-  private final Map<Long, Double> mBlockIdToCRFValue = new ConcurrentHashMapV8<>();
+  private final Map<Long, Float> mBlockIdToCRFValue = new ConcurrentHashMapV8<>();
   // In the range of [0, 1]. Closer to 0, LRFU closer to LFU. Closer to 1, LRFU closer to LRU
-  private final double mStepFactor;
+  private final float mStepFactor;
   // logic time count
   private AtomicLong mLogicTimeCount = new AtomicLong(0L);
 
@@ -53,13 +53,13 @@ public class LFUPromote extends AbstractPromote {
    */
   public LFUPromote(BlockMetadataManagerView managerView, Allocator allocator) {
     super(managerView, allocator);
-    mStepFactor = Configuration.getDouble(PropertyKey.WORKER_PROMOTE_LRFU_STEP_FACTOR);
+    mStepFactor = Configuration.getFloat(PropertyKey.WORKER_PROMOTE_LRFU_STEP_FACTOR);
     Preconditions.checkArgument(mStepFactor >= 0.0 && mStepFactor <= 1);
     for (StorageTierView tier : mManagerView.getTierViews()) {
       for (StorageDirView dir : tier.getDirViews()) {
         for (BlockMeta block : dir.getEvictableBlocks()) {
           mBlockIdToLastUpdateTime.put(block.getBlockId(), 0L);
-          mBlockIdToCRFValue.put(block.getBlockId(), 0.0);
+          mBlockIdToCRFValue.put(block.getBlockId(), 0.0f);
         }
       }
     }
@@ -67,8 +67,9 @@ public class LFUPromote extends AbstractPromote {
 
   @Override
   public List<Long> getBlocksInDescendingOrder() {
-    List<Map.Entry<Long, Double>> sortedCRF = new ArrayList<>(mBlockIdToCRFValue.entrySet());
-    for (Map.Entry<Long, Double> entry : sortedCRF) {
+    updateCRFValue();
+    List<Map.Entry<Long, Float>> sortedCRF = new ArrayList<>(mBlockIdToCRFValue.entrySet());
+    for (Map.Entry<Long, Float> entry : sortedCRF) {
       try {
         long blockId = entry.getKey();
         BlockMeta blockMeta = mManagerView.getBlockMeta(blockId);
@@ -76,30 +77,30 @@ public class LFUPromote extends AbstractPromote {
           int tierOrd =
               mManagerView.getBlockMeta(blockId).getParentDir().getParentTier().getTierOrdinal();
           if (tierOrd == 0) {
-            entry.setValue(entry.getValue() * 2.25);
+            entry.setValue(entry.getValue() + 1.0f);
           } else if (tierOrd == 1) {
-            entry.setValue(entry.getValue() * 1.50);
+            entry.setValue(entry.getValue() + 0.5f);
           }
         }
       } catch (BlockDoesNotExistException e) {
         // TODO(shupeng) remove block
       }
     }
-    Collections.sort(sortedCRF, new Comparator<Map.Entry<Long, Double>>() {
+    Collections.sort(sortedCRF, new Comparator<Map.Entry<Long, Float>>() {
       @Override
-      public int compare(Map.Entry<Long, Double> o1, Map.Entry<Long, Double> o2) {
-        return -Double.compare(o1.getValue(), o2.getValue());
+      public int compare(Map.Entry<Long, Float> o1, Map.Entry<Long, Float> o2) {
+        return -Float.compare(o1.getValue(), o2.getValue());
       }
     });
     List<Long> sortedBlocks = new ArrayList<>();
-    for (Map.Entry<Long, Double> entry : sortedCRF) {
+    for (Map.Entry<Long, Float> entry : sortedCRF) {
       sortedBlocks.add(entry.getKey());
     }
     return sortedBlocks;
   }
 
-  private double calculateAccessWeight(long logicTimeInterval) {
-    return Math.pow(0.5, logicTimeInterval * mStepFactor);
+  private float calculateAccessWeight(long logicTimeInterval) {
+    return (float) (Math.pow(0.5f, logicTimeInterval * mStepFactor));
   }
 
   @Override
@@ -124,9 +125,9 @@ public class LFUPromote extends AbstractPromote {
 
   private void updateCRFValue() {
     long currentLogicTime = mLogicTimeCount.get();
-    for (Map.Entry<Long, Double> entry : mBlockIdToCRFValue.entrySet()) {
+    for (Map.Entry<Long, Float> entry : mBlockIdToCRFValue.entrySet()) {
       long blockId = entry.getKey();
-      double crfValue = entry.getValue();
+      float crfValue = entry.getValue();
       mBlockIdToCRFValue.put(blockId, crfValue
           * calculateAccessWeight(currentLogicTime - mBlockIdToLastUpdateTime.get(blockId)));
       mBlockIdToLastUpdateTime.put(blockId, currentLogicTime);
@@ -140,9 +141,9 @@ public class LFUPromote extends AbstractPromote {
         mBlockIdToCRFValue.put(blockId,
             mBlockIdToCRFValue.get(blockId)
                 * calculateAccessWeight(currentLogicTime - mBlockIdToLastUpdateTime.get(blockId))
-                + 1.0);
+                + 1.0f);
       } else {
-        mBlockIdToCRFValue.put(blockId, 1.0);
+        mBlockIdToCRFValue.put(blockId, 1.0f);
       }
       mBlockIdToLastUpdateTime.put(blockId, currentLogicTime);
     }
