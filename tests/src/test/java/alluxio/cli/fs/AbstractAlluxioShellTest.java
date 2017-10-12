7e933d19b1b5281bd6f6d43db2a0d50f87f32a9a
JiangXiNJU <m18013972874@163.com>
2017-10-12 18:52:28 +0800

    [SMALLFIX] tests/src/test/java/alluxio/cli/fs/AbstractAlluxioShellTest.java

--- tests/src/test/java/alluxio/cli/fs/AbstractAlluxioShellTest.java
+++ tests/src/test/java/alluxio/cli/fs/AbstractAlluxioShellTest.java
@@ -11,11 +11,11 @@
 
 package alluxio.cli.fs;
 
-import alluxio.PropertyKey;
 import alluxio.AlluxioURI;
-import alluxio.LocalAlluxioClusterResource;
-import alluxio.Constants;
 import alluxio.BaseIntegrationTest;
+import alluxio.Constants;
+import alluxio.LocalAlluxioClusterResource;
+import alluxio.PropertyKey;
 import alluxio.SystemOutRule;
 import alluxio.client.ReadType;
 import alluxio.client.WriteType;
@@ -28,9 +28,7 @@ import alluxio.master.LocalAlluxioCluster;
 import alluxio.security.LoginUserTestUtils;
 import alluxio.util.io.BufferUtils;
 import alluxio.util.io.PathUtils;
-
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
@@ -42,6 +40,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * The base class for all the {@link FileSystemShell} test classes.
@@ -87,7 +89,7 @@ public abstract class AbstractAlluxioShellTest extends BaseIntegrationTest {
     FileSystemTestUtils.createByteFile(mFileSystem, "/testFile", WriteType.MUST_CACHE, bytes);
     mFsShell.run("copyToLocal", "/testFile",
         mLocalAlluxioCluster.getAlluxioHome() + "/testFile");
-    Assert.assertEquals(getCommandOutput(new String[] {"copyToLocal", "/testFile",
+    assertEquals(getCommandOutput(new String[] {"copyToLocal", "/testFile",
         mLocalAlluxioCluster.getAlluxioHome() + "/testFile"}), mOutput.toString());
     fileReadTest("/testFile", 10);
   }
@@ -104,7 +106,7 @@ public abstract class AbstractAlluxioShellTest extends BaseIntegrationTest {
     byte[] read = new byte[size];
     fis.read(read);
     fis.close();
-    Assert.assertTrue(BufferUtils.equalIncreasingByteArray(size, read));
+    assertTrue(BufferUtils.equalIncreasingByteArray(size, read));
   }
 
   /**
@@ -262,12 +264,12 @@ public abstract class AbstractAlluxioShellTest extends BaseIntegrationTest {
    * @param size The size of the file
    */
   protected void checkFilePersisted(AlluxioURI uri, int size) throws Exception {
-    Assert.assertTrue(mFileSystem.getStatus(uri).isPersisted());
+    assertTrue(mFileSystem.getStatus(uri).isPersisted());
     mFileSystem.free(uri);
     try (FileInStream tfis = mFileSystem.openFile(uri)) {
       byte[] actual = new byte[size];
       tfis.read(actual);
-      Assert.assertArrayEquals(BufferUtils.getIncreasingByteArray(size), actual);
+      assertArrayEquals(BufferUtils.getIncreasingByteArray(size), actual);
     }
   }
 
@@ -281,7 +283,7 @@ public abstract class AbstractAlluxioShellTest extends BaseIntegrationTest {
   protected void verifyCommandReturnValueAndOutput(int expectedReturnValue, String expectedOutput,
       String... command) {
     int ret = mFsShell.run(command);
-    Assert.assertEquals(expectedReturnValue, ret);
-    Assert.assertTrue(mOutput.toString().contains(expectedOutput));
+    assertEquals(expectedReturnValue, ret);
+    assertTrue(mOutput.toString().contains(expectedOutput));
   }
 }
